397d7ea34a214b6fcc99848810b32e7d23ea3039
SeaDer <d1014181006@gm.lhu.edu.tw>
2020-06-09 18:28:07 +0800

    -新增 自動左右走

--- AutoScript.py
+++ AutoScript.py
@@ -3,6 +3,9 @@ import time
 import pyautogui
 import random
 import json
+from MainWindow import MainWindow
+from PyQt5.QtWidgets import QApplication
+from PyQt5 import sip
 
 key_dict = {"A": 0x1E, "B": 0x30, "C": 0x2E, "D": 0x20, "E": 0x12, "F": 0x21, "G": 0x22, "H": 0x23,
             "I": 0x17, "J": 0x24, "K": 0x25, "L": 0x26, "M": 0x32, "N": 0x31, "O": 0x18, "P": 0x19,
@@ -73,6 +76,7 @@ class AutoScript:
         self.get_script_json()
 
     def init(self):
+        self.left_right_count = 0
         self.script_list = None
 
     def get_script_json(self):
@@ -88,18 +92,36 @@ class AutoScript:
 
     def script_process(self):
         for script in self.script_list:
-            self.keybroad_controller(event=script['event'],
-                                     do_sleep=script['sleep'],
-                                     repeats=script['repeat'])
+            if script['name'] == 'auto_left_and_right':
+                self.auto_left_and_right(do_sleep=script['sleep'])
+            else:
+                self.keybroad_controller(event=script['event'],
+                                         do_sleep=script['sleep'],
+                                         repeats=script['repeat'])
+
+    def auto_left_and_right(self, do_sleep):
+        if self.left_right_count % 2 == 1:
+            print('left')
+            player_teleport("left")
+            time.sleep(do_sleep)
+            # time.sleep(0.5 * random.random())
+        else:
+            print('right')
+            player_teleport("right")
+            time.sleep(do_sleep)
+            # time.sleep(0.5 * random.random())
+        self.left_right_count += 1
+        print('auto_left_and_right: {}'.format(self.left_right_count))
+
 
     def keybroad_controller(self, event, do_sleep, repeats):
         for count in range(repeats):
             PressKey(key_dict[event])
-            time.sleep(10)
+            # time.sleep(10)
             ReleaseKey(key_dict[event])
             time.sleep(do_sleep)
 
-            print('Loop : {}'.format(count))
+            print('event: {}, Loop : {}'.format(event, count))
 
     def execute(self):
         self.buffer_time()
@@ -112,3 +134,9 @@ if __name__ == '__main__':
     obj = AutoScript()
     print(obj.script_list)
     obj.execute()
+    # app = QApplication([])
+    #
+    # main_window = MainWindow().get_main_window()
+    # main_window.setFixedSize(1024, 768)
+    # main_window.show()
+    # sys.exit(app.exec_())
